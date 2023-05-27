37bc686f07b8844b0cb835fc64efce1ae86a3305
Mateusz Preizner <preizner.mateusz@gmail.com>
2023-05-27 14:58:35 +0200

    2 commit

---
+++ src/main/java/pl/sda/zdjavapol131/zdjavapol131/dataProvider/BookIndexH2DataProvider.java
@@ -0,0 +1,14 @@
+package pl.sda.zdjavapol131.zdjavapol131.dataProvider;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookIndexH2DataProvider implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
