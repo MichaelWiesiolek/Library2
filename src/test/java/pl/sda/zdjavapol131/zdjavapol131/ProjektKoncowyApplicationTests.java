3c2163e2660b49848c60f59a4aa424e2a07374a9
Mateusz Preizner <preizner.mateusz@gmail.com>
2023-05-27 14:34:08 +0200

    1 commit

---
+++ src/test/java/pl/sda/zdjavapol131/zdjavapol131/ProjektKoncowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.zdjavapol131.zdjavapol131;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKoncowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
